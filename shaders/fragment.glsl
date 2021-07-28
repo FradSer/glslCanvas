c175ba139b214e29a1e01c46aabc176cafc1e01e
Frad LEE <fradser@gmail.com>
2021-07-28 16:50:41 +0800

    refactor(shader): update fragment 1

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -38,6 +38,35 @@ float lerp(float start, float end, float fraction) {
     return start + ((end - start) * fraction);
 }
 
+// halftone
+// From https://github.com/genekogan/Processing-Shader-Examples/blob/master/TextureShaders/data/halftone.glsl
+// and
+// https://codepen.io/cmalven/pen/mMrEbV Not working now...
+vec3 halftone(vec2 st, float s, vec3 col) {
+    float pixelSize = s;
+	
+	float dx = mod(st.x, pixelSize) - pixelSize*0.5;
+	float dy = mod(st.y, pixelSize) - pixelSize*0.5;
+  float d = min(dx, dy);
+
+	st.x += d ;
+	st.y += d;
+	float bright = col.r  + col.g;
+	
+	float dist = sqrt(dx*dx + dy*dy);
+	float rad = bright * pixelSize * 0.4;
+	float m = step(dist, rad);
+
+	return mix(col,vec3(1.0), m);
+}
+
+vec3 gradient(vec2 st, vec3 beginColor, vec3 endColor) {
+  return vec3(
+         lerp(beginColor.x, endColor.x, st.x),
+         lerp(beginColor.y, endColor.y, st.x),
+         lerp(beginColor.z, endColor.z, st.x));
+}
+
 void main(){
   float n = 100.0;
 
@@ -55,45 +84,18 @@ void main(){
 
   vec3 col = 1.0 - mix(vec3(1.), vec3(0.), vec3(noise(uv*8.0-vec2(u_time/2., u_time/2.0))));
 
-  vec4 beginColor = vec4(0.93, 0, 1, 1);
-  vec4 endColor = vec4(0.21, 0.61, 1, 1);
-
-  vec3 gradient_col = vec3(
-        lerp(beginColor.x, endColor.x, uv.x),
-        lerp(beginColor.y, endColor.y, uv.x),
-        lerp(beginColor.z, endColor.z, uv.x));
+  vec3 gradient_col = gradient(uv, vec3(0.93, 0., 1.), vec3(0.21, 0.61, 1.));
 
   col = mix(col, gradient_col, 0.5);
 
-  // https://github.com/genekogan/Processing-Shader-Examples/blob/master/TextureShaders/data/halftone.glsl
-  // OR
-  // https://codepen.io/cmalven/pen/mMrEbV Not working now...
-  float pixelSize = 0.5;
-	
-	float dx = mod(uvg.x, pixelSize) - pixelSize*0.5;
-	float dy = mod(uvg.y, pixelSize) - pixelSize*0.5;
-  float d = min(dx, dy);
-
-	uvg.x += d ;
-	uvg.y += d;
-	float bright = col.r ;
-	
-	float dist = sqrt(dx*dx + dy*dy);
-	float rad = bright * pixelSize * 0.5;
-	float m = step(dist, rad);
-
-	vec3 halftone_col = mix(gradient_col,vec3(1.0), m);
+	vec3 halftone_col = halftone(uvg, 0.5, col);
 
   vec3 abstract_col = mix(vec3(0), vec3(1, 1, 1), 5.0*vec3(pow(1.0-noise(uv*4.0-vec2(u_time/2., u_time/2.0)),4.0)));
-
   abstract_col = mix(abstract_col, vec3(0.6863, 0.4902, 0.0275), 1.3); // make gradient color 
-
   abstract_col = pow(abstract_col, vec3(1./2.2))*b(1.0); // make color more bright
 
   vec3 final_col = mix(gradient_col, col, 0.5);
-
   final_col = mix(final_col, abstract_col, 0.5);
-
   final_col = mix(final_col, halftone_col, 0.1);
 
   gl_FragColor = vec4(final_col,1.0); // final color
